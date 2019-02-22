9f69f7a97cff4dc74369d24939a0ace0a930f759
amitnyc83 <amit_nyc@live.com>
2019-02-22 15:06:44 -0500

    all test passing

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -3,8 +3,8 @@ class GithubRepo
   attr_reader :name, :url
 
   def initialize(hash)
-    @name = hash["name"]
-    @url = hash["html_url"]
+    @name = hash['name']
+    @url = hash['html_url']
   end
 
 end
